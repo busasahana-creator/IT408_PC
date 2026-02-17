7325c0dcd996f28498edeba68af7bdeff04a48e7
busasahana-creator <busasahana@gmail.com>
2026-02-17 13:53:47 +0530

    Add L3P1 class with display and show methods

---
+++ LAB 3/L3P1.java
@@ -0,0 +1,12 @@
+class L3P1{
+         static int view;
+         public static void display(){
+         	System.out.println(view);
+         }
+         public int show(){
+         	return(view);
+         }
+}
+
+                  
+                    
